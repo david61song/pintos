6989a9b301bad5d4c1e76e088980da86b1e9bbd1
SONG <david61song@protonmail.com>
2021-01-15 23:47:42 +0900

    struct thread added

--- threads/thread.h
+++ threads/thread.h
@@ -89,7 +89,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
+    uint8_t wakeup_tick; /* tick when thread should wake up. */
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
