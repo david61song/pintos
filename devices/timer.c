37898db3208854b52b1ec6a4784d9a73ab4ef426
SONG <david61song@protonmail.com>
2021-01-16 01:02:24 +0900

    alarm-clock modify

--- devices/timer.c
+++ devices/timer.c
@@ -92,8 +92,7 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  thread_sleep(start + ticks);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -165,11 +164,17 @@ timer_print_stats (void)
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
-
+
 /* Timer interrupt handler. */
+/* Called every tick. */
+
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
+  if(check_sleep_thread() == true){
+    //check sleeping thread which needed to wake up..
+  }
+  
   ticks++;
   thread_tick ();
 }
